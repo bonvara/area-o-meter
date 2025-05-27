8d2b7438b975b43e35cfb7fdccbbd6cec1f412c3
Bonvara <bonvara@pm.me>
2025-05-28 01:37:13 +0400

    feat: implement shapes and area calculation

--- examples/use.py
+++ examples/use.py
@@ -2,3 +2,8 @@ from meter import area
 
 print(dir(area))
 area.hello_world()
+
+t = area.Triangle(3, 4, 5)
+print(t.get_area())
+
+s = area.Shape()
--- meter/area.py
+++ meter/area.py
@@ -1,2 +1,32 @@
+from abc import ABC, abstractmethod
+import math
+
 def hello_world():
     print("Arakadabra!")
+
+class Shape(ABC):
+    def get_area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        return math.pi * self.radius ** 2
+    
+class Triangle(Shape):
+    def __init__(self, a, b, c):
+        self.sides = sorted([a, b, c])
+
+    def get_area(self):
+        a, b, c = self.sides
+        if self.is_right():
+            return a * b / 2
+        p = (a + b + c) / 2
+        return math.sqrt(p * (p - a) * (p - b) * (p - c))
+    
+    def is_right(self):
+        a, b, c = self.sides
+        return math.isclose(a**2 + b**2, c**2)
+
